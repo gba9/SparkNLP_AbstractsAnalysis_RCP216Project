127fa4f6ee90e4e9024955d3f1a795cb42bc18ea
Gaelle <gaelleba44@gmail.com>
2025-01-17 10:49:44 +0100

    Add all files to include for the submission

---
+++ Sankey.R
@@ -0,0 +1,47 @@
+library(dplyr)
+library(tidyr)
+library(networkD3)
+setwd("~/Dropbox/CNAM/RCP216/RCP216_PROJECT/2-AbstractsCollection/Abstract_analysis")
+data=read.csv("merged_results_renamed.csv", sep=",", header=TRUE)
+
+
+data <- data %>%
+  mutate(
+    LDA = paste0(LDA, "_LDA"),
+    instructor = paste0(instructor, "_instructor"),
+    doc2vec = paste0(doc2vec, "_doc2vec")
+  )
+
+# Count occurrences of each source-target pair
+result1 <- data %>%
+  count(LDA, instructor, name = "value")
+colnames(result1)<-c("source", "target", "value")
+
+result2<- data %>%
+  count(instructor, doc2vec, name = "value")
+colnames(result2)<-c("source", "target", "value")
+
+result3<- data %>%
+  count(instructor, LDA, name = "value")
+colnames(result3)<-c("source", "target", "value")
+
+
+links<- rbind(result1, result2)
+
+
+names <- data.frame(
+  name=c(as.character(links$source), 
+         as.character(links$target)) %>% unique()
+)
+
+# With networkD3, connection must be provided using id, not using real name like in the links dataframe.. So we need to reformat it.
+links$IDsource <- match(links$source, names$name)-1 
+links$IDtarget <- match(links$target, names$name)-1
+
+
+# Make the Network
+p <- sankeyNetwork(Links = links, Nodes = names,
+                   Source = "IDsource", Target = "IDtarget",
+                   Value = "value", NodeID = "name", 
+                   sinksRight=FALSE, fontSize = 10, nodeWidth = 10)
+p
